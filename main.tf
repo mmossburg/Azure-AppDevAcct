95a737e001e93c40410dc9f02f9744cd038ba5d5
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2021-06-09 15:40:00 -0400

    Create main.tf
    start new subscription/Account

---
+++ main.tf
@@ -0,0 +1,36 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">2.0.0"
+    }
+  }
+}
+
+
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  
+  }
+
+
+# Create a resource group
+resource "azurerm_resource_group" "RGAppDev" {
+  name     = "mossy-AppDev"
+  location = "eastus"
+}
+
+/*  TBD still ned to figure out addressing spaces and stuff
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+ */ 
+  
