41fd2ff9a17796b9455f509263f67fcb5486fae5
Matt Mossburg <mossycastle@gmail.com>
2023-05-10 12:32:15 -0400

    sync it

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_resource_group" "RGAppDev" {
  # TBD still ned to figure out addressing spaces and stuff
 
 # Create a virtual network within the resour_1.1.0.0.-16ce group
-resource "azurerm_virtual_network" "EUS-VNET_10_1_0_0_16" {
+resource "azurerm_virtual_network" "EUS-VNET" {  #_10_1_0_0_16
   name                = "EUS-VNET"
   resource_group_name = azurerm_resource_group.RGAppDev.name
   location            = azurerm_resource_group.RGAppDev.location
@@ -37,7 +37,7 @@ resource "azurerm_virtual_network" "EUS-VNET_10_1_0_0_16" {
   # create subnet configs
   
   resource "azurerm_subnet" "EUS_Subnet" {
-    name "EUS_Subnet_10_1_1_0_24"
+    name                 = "EUS_Subnet_10_1_1_0_24"
     resource_group_name  = azurerm_resource_group.RGAppDev.name
     virtual_network_name = azurerm_virtual_network.EUS_VNET.name
     address_prefixes     = ["10.1.1.0/24"]
