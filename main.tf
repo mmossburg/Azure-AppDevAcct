30b345787c5cd1ee4b73beb4d0d3f47a6022812f
MossyCastle <46568873+MossyCastle@users.noreply.github.com>
2023-05-10 11:27:06 -0400

    Update main.tf
    update vnet

--- main.tf
+++ main.tf
@@ -24,13 +24,16 @@ resource "azurerm_resource_group" "RGAppDev" {
   location = "eastus"
 }
 
-/*  TBD still ned to figure out addressing spaces and stuff
+ TBD still ned to figure out addressing spaces and stuff
 
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
- */ 
+  address_space       = ["10.1.0.0/16"]
+
+  
+  # create subnet configs
+  
   
